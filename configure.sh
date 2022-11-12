ad4df6d5c83d326627fdbe060182cc966fcbed1c
yy13hd <112591644+yy13hd@users.noreply.github.com>
2022-11-11 21:43:31 -0500

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8080,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "93c19849-af8c-4255-9bcf-62134cd3b037",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/lawrkrc"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
